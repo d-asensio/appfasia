72a59038f869b71e6a8c9b8fa5c200998dd769f4
David Asensio Cañas <david.asensio@icloud.com>
2022-11-25 19:52:03 +0100

    Write words in textarea when clicking them

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import './App.css';
 import 'normalize.css';
+import { useCallback, useState } from 'react'
+
+const initialState = {
+  text: '',
+  words: [
+    'Yo',
+    'Quiero',
+    'Pancake'
+  ]
+}
 
 function App() {
+  const [text, setText] = useState(initialState.text)
+  const [words] = useState(initialState.words)
+
+  const handleWordClick = useCallback(e => {
+    const clickedWord = e.target.innerText
+
+    setText(`${text} ${clickedWord}`)
+  }, [text])
+
   return (
     <div className="App">
-      <textarea className='input-area'></textarea>
+      <textarea className='input-area' value={text} />
       <div className='word-options'>
-        <button>Yo</button>
-        <button>Quiero</button>
-        <button>Pancake</button>
+        {words.map(word => (
+          <button key={word} onClick={handleWordClick}>{word}</button>
+        ))}
       </div>
     </div>
   );
