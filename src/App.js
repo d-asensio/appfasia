aabd603fecde81ba388506d1fcae09a4497c029f
David Asensio Cañas <david.asensio@icloud.com>
2022-11-25 20:58:55 +0100

    Add spanish verbs to the mix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import Button from '@mui/joy/Button'
-import { CssVarsProvider } from '@mui/joy/styles';
-import './App.css';
-import 'normalize.css';
+import { CssVarsProvider } from '@mui/joy/styles'
+import './App.css'
+import 'normalize.css'
 import { useCallback, useState } from 'react'
 import { useSpeechSynthesis } from 'react-speech-kit'
-import { CssBaseline, IconButton } from '@mui/joy';
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
-import VolumeUpIcon from '@mui/icons-material/VolumeUp';
-import VolumeOffIcon from '@mui/icons-material/VolumeOff';
-import styled from '@emotion/styled';
+import { CssBaseline, IconButton } from '@mui/joy'
+import ContentCopyIcon from '@mui/icons-material/ContentCopy'
+import VolumeUpIcon from '@mui/icons-material/VolumeUp'
+import VolumeOffIcon from '@mui/icons-material/VolumeOff'
+import styled from '@emotion/styled'
 import { useCopyToClipboard } from 'react-use'
-
-const initialState = {
-  text: '',
-  words: [
-    'Yo',
-    'Quiero',
-    'Pancake',
-    'Supercalifragilisticoespialidoso',
-  ]
-}
+import { initialState } from './InitialState'
 
 const Actions = styled.div`
   position: absolute;
@@ -27,14 +18,24 @@ const Actions = styled.div`
   top: 1rem;
   display: flex;
   gap: 0.5rem;
-`;
+`
 
-function App() {
-  const { speak } = useSpeechSynthesis();
+const WordOptions = styled.div`
+  margin: 1rem;
+  text-align: start;
+  
+  > * {
+    margin-right: 0.5rem;
+    margin-bottom: 0.5rem;
+  }
+`
+
+function App () {
+  const { speak } = useSpeechSynthesis()
   const [text, setText] = useState(initialState.text)
   const [words] = useState(initialState.words)
-  const [speakEnabled, setSpeakEnabled] = useState(true);
-  const [, copyToClipboard] = useCopyToClipboard();
+  const [speakEnabled, setSpeakEnabled] = useState(true)
+  const [, copyToClipboard] = useCopyToClipboard()
 
   const handleWordClick = useCallback(e => {
     const clickedWord = e.target.innerText
@@ -58,46 +59,45 @@ function App() {
 
   return (
     <CssVarsProvider>
-      <CssBaseline />
+      <CssBaseline/>
       <div className="App">
         <Actions>
           <IconButton
             size="lg"
-            variant='outlined'
-            color='neutral'
+            variant="outlined"
+            color="neutral"
             onClick={() => setSpeakEnabled(!speakEnabled)}
           >
             {speakEnabled
-              ? <VolumeUpIcon />
-              : <VolumeOffIcon />}
+              ? <VolumeUpIcon/>
+              : <VolumeOffIcon/>}
           </IconButton>
           <IconButton
             size="lg"
-            variant='outlined'
-            color='neutral'
+            variant="outlined"
+            color="neutral"
             onClick={handleCopyCLick}
           >
-            <ContentCopyIcon />
+            <ContentCopyIcon/>
           </IconButton>
         </Actions>
-        <textarea className='input-area' value={text} onChange={handleTextareaChange}/>
-        <div className='word-options'>
+        <textarea className="input-area" value={text} onChange={handleTextareaChange}/>
+        <WordOptions>
           {words.map(word => (
-            <Button   
+            <Button
               size="lg"
               color="primary"
-              variant="solid" 
+              variant="solid"
               key={word}
               onClick={handleWordClick}
             >
               {word}
             </Button>
           ))}
-        </div>
+        </WordOptions>
       </div>
     </CssVarsProvider>
-  );
+  )
 }
 
-
-export default App;
+export default App
