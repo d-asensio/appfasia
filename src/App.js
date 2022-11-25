a4a3405e9f826f961fefba6926a898e022b84d53
Mario Sánchez <mr.mario.sm@gmail.com>
2022-11-25 20:39:37 +0100

    Add action buttons

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
+import Button from '@mui/joy/Button'
+import { CssVarsProvider } from '@mui/joy/styles';
 import './App.css';
 import 'normalize.css';
 import { useCallback, useState } from 'react'
 import { useSpeechSynthesis } from 'react-speech-kit'
+import { CssBaseline, IconButton } from '@mui/joy';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import VolumeUpIcon from '@mui/icons-material/VolumeUp';
+import VolumeOffIcon from '@mui/icons-material/VolumeOff';
+import styled from '@emotion/styled';
 
 const initialState = {
   text: '',
   words: [
     'Yo',
     'Quiero',
-    'Pancake'
+    'Pancake',
+    'Supercalifragilisticoespialidoso',
   ]
 }
+
+const Actions = styled.div`
+  position: absolute;
+  right: 1rem;
+  top: 1rem;
+  display: flex;
+  gap: 0.5rem;
+`;
 
 function App() {
   const { speak } = useSpeechSynthesis();
   const [text, setText] = useState(initialState.text)
   const [words] = useState(initialState.words)
+  const [speakEnabled, setSpeakEnabled] = useState(true);
 
   const handleWordClick = useCallback(e => {
     const clickedWord = e.target.innerText
@@ -29,18 +46,50 @@ function App() {
     const writtenText = e.target.value
 
     setText(writtenText)
-  }, [text])
+  }, [])
 
   return (
-    <div className="App">
-      <textarea className='input-area' value={text} onChange={handleTextareaChange}/>
-      <div className='word-options'>
-        {words.map(word => (
-          <button key={word} onClick={handleWordClick}>{word}</button>
-        ))}
+    <CssVarsProvider>
+      <CssBaseline />
+      <div className="App">
+        <Actions>
+          <IconButton
+            size="lg"
+            variant='outlined'
+            color='neutral'
+            onClick={() => setSpeakEnabled(!speakEnabled)}
+          >
+            {speakEnabled
+              ? <VolumeUpIcon />
+              : <VolumeOffIcon />}
+          </IconButton>
+          <IconButton
+            size="lg"
+            variant='outlined'
+            color='neutral'
+            onClick={() => setSpeakEnabled(!speakEnabled)}
+          >
+            <ContentCopyIcon />
+          </IconButton>
+        </Actions>
+        <textarea className='input-area' value={text} onChange={handleTextareaChange}/>
+        <div className='word-options'>
+          {words.map(word => (
+            <Button   
+              size="lg"
+              color="primary"
+              variant="solid" 
+              key={word}
+              onClick={handleWordClick}
+            >
+              {word}
+            </Button>
+          ))}
+        </div>
       </div>
-    </div>
+    </CssVarsProvider>
   );
 }
+
 
 export default App;
