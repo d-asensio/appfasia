e2e71e80a91a2859015cadf8a99403af460a5f9e
David Asensio Cañas <david.asensio@icloud.com>
2022-11-26 09:47:50 +0100

    Add tabs and search box components without functuionality

--- src/App.js
+++ src/App.js
@@ -4,16 +4,27 @@ import './App.css'
 import 'normalize.css'
 import { useCallback, useState } from 'react'
 import { useSpeechSynthesis } from 'react-speech-kit'
-import { CssBaseline, IconButton, Textarea as JoyTextarea, Tooltip } from '@mui/joy'
+import { CssBaseline, IconButton, Textarea as JoyTextarea, TextField, Tooltip } from '@mui/joy'
+
 import ContentCopyIcon from '@mui/icons-material/ContentCopy'
 import VolumeUpIcon from '@mui/icons-material/VolumeUp'
 import VolumeOffIcon from '@mui/icons-material/VolumeOff'
 import PlayCircleIcon from '@mui/icons-material/PlayCircle'
 import DeleteIcon from '@mui/icons-material/Delete'
+import SearchIcon from '@mui/icons-material/Search'
+
 import styled from '@emotion/styled'
 import { useCopyToClipboard } from 'react-use'
 import { initialState } from './InitialState'
-import { Box } from '@mui/system'
+
+import Box from '@mui/joy/Box'
+import Tabs from '@mui/joy/Tabs'
+import TabList from '@mui/joy/TabList'
+import Tab, { tabClasses } from '@mui/joy/Tab'
+import TabPanel from '@mui/joy/TabPanel'
+import CloseIcon from '@mui/icons-material/Close'
+import AddIcon from '@mui/icons-material/Add'
+
 
 const Textarea = styled(JoyTextarea)`
   margin: 1rem;
@@ -35,6 +46,148 @@ const WordOptions = styled.div`
     margin: 0.25rem;
   }
 `
+
+function EditorTabs () {
+  const [index, setIndex] = useState(0)
+
+  if (!featureFlags.isEnabled('TABS')) return null
+
+  return (
+    <Tabs
+      aria-label="Pipeline"
+      value={index}
+      onChange={(event, value) => setIndex(value)}
+      sx={{ '--Tabs-gap': '0px' }}
+    >
+      <TabList
+        variant="plain"
+        sx={{
+          width: '100%',
+          pt: 2,
+          alignItems: 'flex-end',
+          alignSelf: 'flex-start',
+          [`& .${tabClasses.root}`]: {
+            bgcolor: 'transparent',
+            boxShadow: 'none',
+            '&:hover': {
+              bgcolor: 'transparent'
+            },
+            [`&.${tabClasses.selected}`]: {
+              color: 'primary.plainColor',
+              fontWeight: 'lg',
+              '&:before': {
+                content: '""',
+                display: 'block',
+                position: 'absolute',
+                zIndex: 1,
+                bottom: '-1px',
+                left: 'var(--List-item-paddingLeft)',
+                right: 'var(--List-item-paddingRight)',
+                height: '3px',
+                borderTopLeftRadius: '3px',
+                borderTopRightRadius: '3px',
+                bgcolor: 'primary.500'
+              }
+            }
+          }
+        }}
+      >
+        <Tab
+          sx={{
+            flexGrow: 0,
+            display: 'flex',
+            gap: '0.5em'
+          }}
+        >
+          Pestaña 1{' '}
+          <IconButton size="sm" variant="plain" color="neutral">
+            <CloseIcon/>
+          </IconButton>
+        </Tab>
+        <Tab
+          sx={{
+            flexGrow: 0,
+            display: 'flex',
+            gap: '0.5em'
+          }}
+        >
+          Pestaña 2{' '}
+          <IconButton size="sm" variant="plain" color="neutral">
+            <CloseIcon/>
+          </IconButton>
+        </Tab>
+        <Tab
+          sx={{
+            flexGrow: 0,
+            display: 'flex',
+            gap: '0.5em'
+          }}
+        >
+          Pestaña 3{' '}
+          <IconButton size="sm" variant="plain" color="neutral">
+            <CloseIcon/>
+          </IconButton>
+        </Tab>
+        <Box
+          sx={{
+            alignSelf: 'center',
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'flex-end'
+          }}
+        >
+          <IconButton size="sm" variant="plain" color="neutral">
+            <AddIcon/>
+          </IconButton>
+        </Box>
+      </TabList>
+      <Box
+        sx={(theme) => ({
+          '--bg': theme.vars.palette.background.level3,
+          height: '1px',
+          background: 'var(--bg)',
+          boxShadow: '0 0 0 100vmax var(--bg)',
+          clipPath: 'inset(0 -100vmax)'
+        })}
+      />
+      <Box
+        sx={(theme) => ({
+          '--bg': theme.vars.palette.background.surface,
+          background: 'var(--bg)',
+          boxShadow: '0 0 0 100vmax var(--bg)',
+          clipPath: 'inset(0 -100vmax)'
+          // px: 4,
+          // py: 2
+        })}
+      >
+        <TabPanel value={0}></TabPanel>
+        <TabPanel value={1}></TabPanel>
+        <TabPanel value={2}></TabPanel>
+      </Box>
+    </Tabs>
+  )
+}
+
+function SearchBox () {
+  if (!featureFlags.isEnabled('SEARCH_BOX')) return null
+
+  return (
+    <div
+      style={{
+        padding: '0.5rem',
+        width: '300px'
+      }}
+    >
+      <TextField
+        label="Buscar"
+        placeholder="Busca una palabra..."
+        endDecorator={
+          <SearchIcon/>
+        }
+      />
+    </div>
+  )
+}
 
 function App () {
   const { speak } = useSpeechSynthesis()
@@ -71,6 +224,7 @@ function App () {
     <CssVarsProvider>
       <CssBaseline/>
       <div className="App">
+        <EditorTabs/>
         <Textarea
           size="lg"
           value={text}
@@ -138,6 +292,7 @@ function App () {
             </Box>
           }
         />
+        <SearchBox/>
         <WordOptions>
           {categories.map(({ name, words, color }) => (
             <div>
