cb417d086ecd94ac54343734d000ea68540c9744
David Asensio Cañas <david.asensio@icloud.com>
2022-11-27 18:36:34 +0100

    Implement editorByIdSelector

--- src/entities/editor/selectors.js
+++ src/entities/editor/selectors.js
@@ -1,9 +1,20 @@
 import { createSelector } from 'reselect'
 import { keys } from 'ramda'
+import { createCachedSelector } from 're-reselect'
 
 const editorsByIdSelector = ({ editorsById }) => editorsById || {}
 
 export const editorIdListSelector = createSelector(
   editorsByIdSelector,
   editorsById => keys(editorsById)
+)
+
+const editorIdParam = (_, editorId) => editorId
+
+export const editorByIdSelector = createCachedSelector(
+  editorIdParam,
+  editorsByIdSelector,
+  (editorId, editorsById) => editorsById[editorId] || null
+)(
+  editorIdParam
 )
--- src/entities/editor/selectors.test.js
+++ src/entities/editor/selectors.test.js
@@ -1,4 +1,4 @@
-import { editorIdListSelector } from './selectors'
+import { editorByIdSelector, editorIdListSelector } from './selectors'
 
 describe('editorIdListSelector', () => {
   it('should return a list of editor ids', () => {
@@ -23,5 +23,32 @@ describe('editorIdListSelector', () => {
     const result = editorIdListSelector(state)
 
     expect(result).toStrictEqual([])
+  })
+})
+
+describe('editorByIdSelector', () => {
+  it('should return the data of an editor given its id', () => {
+    const editorData = {
+      name: 'Editor name',
+      content: 'editor content'
+    }
+
+    const state = {
+      editorsById: {
+        'an-editor-id': editorData
+      }
+    }
+
+    const result = editorByIdSelector(state, 'an-editor-id')
+
+    expect(result).toStrictEqual(editorData)
+  })
+
+  it('should return null when the provided id does not belong to any editor', () => {
+    const state = {}
+
+    const result = editorByIdSelector(state, 'an-unknown-editor')
+
+    expect(result).toEqual(null)
   })
 })
