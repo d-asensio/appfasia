87618c39036d9b11b0607dd89656f16b47e8f173
David Asensio Cañas <david.asensio@icloud.com>
2022-11-27 17:33:39 +0100

    Define interface for word-related selectors

---
+++ src/entities/words/selectors.js
@@ -0,0 +1,8 @@
+import { createSelector } from 'reselect'
+import { createCachedSelector } from 're-reselect'
+
+export const wordCategoryIdListSelector = () => []
+
+export const wordCategoryById = () => ({ id: '', name: '', color: '#fff' })
+
+export const wordListByCategoryId = () => []
