d7105cc16867fc5aa3b36e5c0b6fba2f6b132a97
David Asensio Cañas <david.asensio@icloud.com>
2022-11-29 11:14:44 +0100

    Remove SearchBox and CSS cleanup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import { CssVarsProvider } from '@mui/joy/styles'
-import './App.css'
-import 'normalize.css'
 import { CssBaseline } from '@mui/joy'
-import { SearchBox } from './components/SearchBox'
 import { WordList } from './components/WordList'
 
 function App () {
   return (
     <CssVarsProvider>
       <CssBaseline/>
-      <div className="App">
-        <SearchBox/>
-        <WordList />
-      </div>
+      <WordList />
     </CssVarsProvider>
   )
 }
